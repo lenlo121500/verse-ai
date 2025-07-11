d8e0ecf9468f1352e0127325689fc6794ea96dda
lenlo121500 <raulc8808@gmail.com>
2025-07-12 01:59:59 +0800

    feat: added sidebar component

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,11 +1,43 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { Menu, X } from "lucide-react";
+import React, { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import { SignIn, useUser } from "@clerk/clerk-react";
 
 const Layout = () => {
-  return (
-    <div>
-      <h1>Layout</h1>
-      <Outlet />
+  const [sidebar, setSidebar] = useState(false);
+  const { user } = useUser();
+  return user ? (
+    <div className="flex flex-col items-start justify-start h-screen">
+      <nav className="w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200">
+        <Link to={"/"}>
+          <h1 className="text-2xl font-extrabold tracking-tight text-primary flex items-center space-x-1">
+            <span className="text-black">Verse</span>
+            <span>AI</span>
+          </h1>
+        </Link>
+        {sidebar ? (
+          <X
+            className="size-6 text-gray-600 sm:hidden"
+            onClick={() => setSidebar(false)}
+          />
+        ) : (
+          <Menu
+            className="size-6 text-gray-600 sm:hidden"
+            onClick={() => setSidebar(true)}
+          />
+        )}
+      </nav>
+      <div className="flex-1 w-full flex h-[calc(100vh-64px)]">
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
+        <div className="flex-1 bg-[#F4F7FB]">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <SignIn />
     </div>
   );
 };
