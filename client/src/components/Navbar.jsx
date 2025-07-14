e705d6f180f1a7f3b2a862edba4ed1a74f8bb58a
lenlo121500 <raulc8808@gmail.com>
2025-07-15 04:09:46 +0800

    style: reduce z-index of the parent div

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed z-50 w-full backdrop-blur-2xl">
+    <div className="fixed z-5 w-full backdrop-blur-2xl">
       <div className="flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32">
         {/* Logo */}
         <ScrollLink
